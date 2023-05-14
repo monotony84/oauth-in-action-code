7e9b8ad32776ac30dc73c9ff3a921e62154d3be6
monotony84 <monotony84.d@gmail.com>
2023-05-14 15:37:13 +0900

    ch-3-1:implement callback

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -46,7 +46,7 @@ app.get('/authorize', function(req, res){
 	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
 		response_type: 'code',
 		client_id: client.client_id,
-		redirect_uris: client.redirect_uris[0],
+		redirect_uri: client.redirect_uris[0],
 		state: state
 	})
 	
@@ -56,10 +56,48 @@ app.get('/authorize', function(req, res){
 
 app.get('/callback', function(req, res){
 
-	/*
-	 * Parse the response from the authorization server and get a token
-	 */
-	
+	if (req.query.error) {
+		res.render('error',{error: req.query.error});
+		return;
+	};
+
+	if (req.query.state != state) {
+		console.log('State DOES NOT MATCH: expected %s got %s', state, req.query.state);
+		res.render('error', {error: 'State value did not mathc'});
+		return;
+	}
+
+	var code = req.query.code;
+
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint,
+	{
+		body: form_data,
+		headers: headers
+	});
+
+	console.log('Requesting access token for code %s', code);
+
+	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+		var body = JSON.parse(tokRes.getBody());
+		
+		access_token = body.access_token;
+		console.log('Got access token: %s', access_token);
+
+		res.render('index', {access_token: access_token, scope: scope});
+	} else {
+		res.render('error', {error: 'Unable to fetch access token, serverresponse: ' + tokRes.statusCode});
+	}
 });
 
 app.get('/fetch_resource', function(req, res) {
