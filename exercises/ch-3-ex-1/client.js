207f2e0db9ad079abccf788d0ca8464b1056f8c5
monotony84 <monotony84.d@gmail.com>
2023-05-14 16:07:24 +0900

    ch-3-1:implement fetch protected resource

--- exercises/ch-3-ex-1/client.js
+++ exercises/ch-3-ex-1/client.js
@@ -102,10 +102,28 @@ app.get('/callback', function(req, res){
 
 app.get('/fetch_resource', function(req, res) {
 
-	/*
-	 * Use the access token to call the resource server
-	 */
+	if (!access_token) {
+		res.render('error', { error: 'Missing access token.'});
+		return;
+	}
+
+	console.log('Making request with access token %s', access_token);
+
+	var headers = {
+		'Authorization': 'Bearer ' + access_token
+	};
+
+	var resource =  request('POST', protectedResource, {headers: headers});
 	
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody());
+
+		res.render('data', {resource: body});
+		return;
+	} else {
+		res.render('error', {error: 'Server returned response code: ' + resource.statusCode});
+		return;
+	}
 });
 
 var buildUrl = function(base, options, hash) {
